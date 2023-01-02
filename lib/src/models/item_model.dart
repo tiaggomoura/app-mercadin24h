428eef59b338d87b87d1bcb756c58ce464fae159
tiaggomoura <tiago.rodrigues.dev@gmail.com>
2023-01-02 10:44:06 -0300

    WIP: ajustes

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,14 +1,27 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'item_model.g.dart';
+
+@JsonSerializable()
 class ItemModel {
+  String id;
+  @JsonKey(name: 'title')
   String itemName;
+  @JsonKey(name: 'picture')
   String imgUrl;
   String unit;
   double price;
   String description;
 
   ItemModel(
-      {required this.description,
+      {this.id = '',
+      required this.description,
       required this.imgUrl,
       required this.unit,
       required this.price,
       required this.itemName});
+
+  factory ItemModel.fromJson(Map<String, dynamic> json) => _$ItemModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ItemModelToJson(this);
 }
